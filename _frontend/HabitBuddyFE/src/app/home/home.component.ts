f161700a01b54b2f83796bc07ab8f81f563ab293
dejana-st <dejana.stanic@gmail.com>
2020-11-10 12:43:52 +0100

    Front: auth + dashboard

---
+++ _frontend/HabitBuddyFE/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../_services/user.service';
+import { TokenStorageService } from '../_services/token-storage.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  content: string;
+  isLoggedIn = false;
+  username: string;
+
+
+  constructor(private tokenStorageService: TokenStorageService, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getPublicContent().subscribe(
+      data => {
+        this.content = data;
+      },
+      err => {
+        this.content = JSON.parse(err.error).message;
+      }
+    );
+  }
+
+}
