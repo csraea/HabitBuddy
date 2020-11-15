4b4b134f7aa3547db1cb08eb245f51121f4b8b21
LyudaK <48889293+LyudaK@users.noreply.github.com>
2020-11-15 12:26:46 +0100

    Add files via upload

--- src/main/java/com/kamer/springbootuserregistration/config/WebSecurityConfig.java
+++ src/main/java/com/kamer/springbootuserregistration/config/WebSecurityConfig.java
@@ -18,9 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @AllArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private UserService userService;
+	private final UserService userService;
 
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
